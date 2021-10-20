d3606381c8584fa2623fb22b98ba761baebcff94
SamSepiool <84787501+SamSepiool@users.noreply.github.com>
2021-10-20 20:18:02 +0200

    faqs style fix

--- index.php
+++ index.php
@@ -26,22 +26,22 @@ $faqs= [
     [
         "faq" => "Perché il mio account è associato a un paese?",
         "answer" => "<p>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</p>
-        <ol class='list_nested'>
-        <li>
-            La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per  i consumatori tramite una delle due società seguenti:
-                <ol>
-                    <li>
-                        Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
-                    </li>
-                    <li>
-                        Google LLC, con sede negli Stati Uniti, per il resto del mondo.
-                    </li>
-                </ol>
-        </li>
-        <li>
-            La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
-        </li>
-        </ol>
+            <ol class='list_nested'>
+                <li>
+                    La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per  i consumatori tramite una delle due società seguenti:
+                        <ol>
+                            <li>
+                                Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+                            </li>
+                            <li>
+                                Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+                            </li>
+                        </ol>
+                </li>
+                <li>
+                    La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+                </li>
+            </ol>
 
         <p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>
         
@@ -81,7 +81,14 @@ $faqs= [
     <body>
 
         <header>
-            
+            <div class="top">
+
+                <img class='logo' src="img/googlelogo_clr_74x24px.svg" alt="">
+                    
+                <a href="#">Privacy e termini</a>
+                
+              
+            </div>
 
         </header>
 
